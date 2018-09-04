a6b3dc9c160d1eab97915f4ccdd1f3a25b55d109
Nik Istrefi <nik@uniteit.ch>
2018-09-05 01:07:43 +0200

    Add FileAgent. this class handles all file transfers

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Model/FileAgent.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.IO;
+
+namespace ZbW.Testing.Dms.Client.Repositories
+{
+    /// <summary>
+    /// File worker, contains all the logic which is needed by <see cref="ZbW.Testing.Dms.Client.ViewModels.DocumentDetailViewModel"/>
+    /// </summary>
+    internal class FileAgent
+    {
+        // file & folder metas
+        private DateTime _startTime;
+        private string _issuer;
+        private int _jobId;
+        private string _srcDirectory;
+        private string _targetDir;
+        private string _itemType;
+        private string _itemFullName;
+        private long _itemLength;
+    }
+}
